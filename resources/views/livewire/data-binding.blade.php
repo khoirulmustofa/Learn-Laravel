a1591025d3a6484815ee6128c2f379ae51035cbe
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2024-08-16 18:45:44 +0700

    Breze

--- resources/views/livewire/data-binding.blade.php
+++ resources/views/livewire/data-binding.blade.php
@@ -51,7 +51,7 @@
 
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
             
             <tbody>
                 @foreach ($users as $user)
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-use App\Livewire\Clicker;
-use App\Livewire\DataBinding;
-use App\Http\Controllers\GuardianStudentController;
-use App\Http\Controllers\ManyToManyController;
-use App\Http\Controllers\OneToManyController;
-use App\Http\Controllers\OneToOneController;
-use App\Http\Controllers\PresensiController;
-use App\Livewire\Todo;
-use App\Http\Controllers\StudentsController;
-use App\Models\Asrama;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -23,21 +12,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-    Route::get('/clicker', Clicker::class);
-
-
-    Route::get('/databinding', DataBinding::class);
-
-    
 require __DIR__.'/auth.php';
-Route::get('/presensi', [PresensiController::class, 'index']);
-
-
-Route::get('/todo', Todo::class);
-
-
-Route::get('/guardian-student', [GuardianStudentController::class, 'index']);
-
-// Route::get('/students', [StudentsController::class, 'index'])->name('students.index');
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
